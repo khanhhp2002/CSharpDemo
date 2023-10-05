de81fa7b27ac15fee77227e776179050f2068fba
khanhbq_he2002 <khanhbui1560@gmail.com>
2023-10-05 22:18:31 +0700

    Khanh Update

---
+++ CS50/GetUserInput.cs
@@ -0,0 +1,35 @@
+﻿namespace CS50
+{
+    public class GetUserInput
+    {
+        public static int Get_Int(string message)
+        {
+            int tmp;
+            do
+            {
+                Console.Write(message);
+            } while (!int.TryParse(Console.ReadLine(), out tmp));
+            return tmp;
+        }
+
+        public static float Get_Float(string message)
+        {
+            float tmp;
+            do
+            {
+                Console.Write(message);
+            } while (!float.TryParse(Console.ReadLine(), out tmp));
+            return tmp;
+        }
+
+        public static string Get_String(string message)
+        {
+            string tmp;
+            do{
+                Console.Write(message);
+                tmp = Console.ReadLine();
+            } while (string.IsNullOrEmpty(tmp));
+            return tmp;
+        }
+    }
+}
---
+++ CS50/Math.cs
@@ -0,0 +1,68 @@
+﻿namespace CS50
+{
+    public class Math
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static float Divide(int a, int b)
+        {
+            return (float)a / b;
+        }
+
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static int Modulo(int a, int b)
+        {
+            return a % b;
+        }
+
+        public static int Power(int a, int b)
+        {
+            int result = 1;
+            for (int i = 0; i < b; i++)
+            {
+                result *= a;
+            }
+            return result;
+        }
+
+        public static int Factorial(int a)
+        {
+            int result = 1;
+            for (int i = 1; i <= a; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+
+        public static int GCD(int a, int b)
+        {
+            int tmp;
+            while (b != 0)
+            {
+                tmp = a % b;
+                a = b;
+                b = tmp;
+            }
+            return a;
+        }
+
+        public static int LCM(int a, int b)
+        {
+            return a * b / GCD(a, b);
+        }
+
+    }
+}
